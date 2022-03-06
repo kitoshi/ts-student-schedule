d149c8608462ff56314e0ae88ac954e4eee13820
kitoshi <kitoshi.charlton@gmail.com>
2022-03-06 14:34:15 -0800

    noooo i put in the package lock

--- courses.ts
+++ courses.ts
@@ -1,16 +1,29 @@
 
-type courses = Object
+interface Courses {
+  classes: Array<{
+    name: string;
+    number: string;
+    description: string;
+    credits: string;
+    prerequisites: string;
+  }>
+}
 
-function CourseAdd(obj: courses) {
-  for (const item in obj) {
-    document.querySelector('ul').innerHTML += `<li>${obj[item]}</li>`
+function CourseAdd(obj: Courses) {
+  for (const item in obj.classes) {
+    document.querySelector('table').innerHTML += `<tr><td>${obj.classes[item].name}</td><td>${obj.classes[item].number}</td><td>${obj.classes[item].description}</td><td>${obj.classes[item].credits}</td><td>${obj.classes[item].prerequisites}</td></tr>`
   }
     
 }
 
 
   
-const fetchPromise = fetch('http://localhost:8081/courselist')
-fetchPromise.then(res => {
-  CourseAdd(res.body)
-})
+
+async function fetchCourses() {
+  const response = await fetch('http://localhost:8081/courselist')
+  const courses = await response.json()
+  console.log(courses.classes)
+  CourseAdd(courses)
+}
+
+fetchCourses()
